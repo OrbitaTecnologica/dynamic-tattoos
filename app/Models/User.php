7d19da030d18e713eecaea4e3ad27a1dd9bcce85
Cristian Rodriguez <crodriguez@fustibus.com>
2026-06-11 12:18:08 +0200

    feat(auth): User implementa MustVerifyEmail

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,12 +8,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Cashier\Billable;
 use Laravel\Sanctum\HasApiTokens;
 
-final class User extends Authenticatable
+final class User extends Authenticatable implements MustVerifyEmail
 {
     use Billable;
     use HasApiTokens;
